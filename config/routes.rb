bc77eac88d7c87e0b60da06e01bc084a6b47f20e
tenchooooo <tenchooo.227@gmail.com>
2022-04-10 07:10:07 +0000

    destroyまで

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resources :books
-end
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
+ end

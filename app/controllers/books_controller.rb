bfad8c13a1dca4c3a059de2c047e89f7cb109272
tenchooooo <tenchooo.227@gmail.com>
2022-04-11 02:15:40 +0000

    flashmessage前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,13 +22,15 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
